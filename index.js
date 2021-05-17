94e28538a15902b97357ff7152a3bfbfd56805eb
santiagorobinet <santiagorobinet@gmail.com>
2021-05-17 16:33:20 +0200

    add automated search

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ const { chromium } =  require('playwright');
 async function search(){
     const browser = await chromium.launch({ headless: false, slowMo: 50 });
     const page = await browser.newPage();
+
+    await page.goto('https://web.gencat.cat/ca/inici');
+    await page.fill('#cercadorOcultGoogle', 'agenda cultural');
+    await page.press('#cercadorOcultGoogle', 'Enter');
 }
+
+search();
